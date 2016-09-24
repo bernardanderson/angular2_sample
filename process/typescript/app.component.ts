2c1e43d14a7ec8b25fbb868dcc3e8b4174687e2c
Bernard Anderson <bernardanderson@gmail.com>
2016-09-24 12:45:23 -0500

    Spilt page view module from bootstrap page The page view 'app.component' was created by spliting that info from the bootstrap page and into it's own module.

---
+++ process/typescript/app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from 'angular2/core';
+
+// Decorator contains information about the controller
+//  (Typescript addition, ES7 feature)
+@Component({
+    selector: 'my-app', 
+    template: '<h1>Welcome to my App</h1>'
+})
+
+// Controller for the module
+export class AppComponent {
+
+}
--- process/typescript/boot.ts
+++ process/typescript/boot.ts
@@ -1,17 +1,5 @@
-import {Component} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser'; // Initializes the application
-
-// Decorator contains information about the controller
-//  (Typescript addition, ES7 feature)
-@Component({
-    selector: 'my-app', 
-    template: '<h1>Welcome to my App</h1>'
-})
-
-// Controller for the module
-class AppComponent {
-
-}
+import {AppComponent} from './app.component';  // Loads the app.component.js module
 
 // Loads the function.  Looks for the selector in the 'Component' (as a HTML tag in the index.html)
 //  and binds the content to it.
