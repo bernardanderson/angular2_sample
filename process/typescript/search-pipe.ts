209915a959ad52062c622f7c123a7233e8b625ab
Bernard Anderson <bernard.anderson@gmail.com>
2016-09-25 21:59:55 -0500

    Example of filtering method in pipe

--- process/typescript/search-pipe.ts
+++ process/typescript/search-pipe.ts
@@ -5,5 +5,10 @@ import {Pipe} from 'angular2/core';
 })
 
 export class searchPipe {
-
+  transform(pipeData, [pipeModifier]){
+    return pipeData.filter((eachItem) => {
+      return eachItem['name'].toLowerCase().includes(pipeModifier.toLowerCase()) || 
+             eachItem['reknown'].toLowerCase().includes(pipeModifier.toLowerCase());
+    });
+  }
 }
